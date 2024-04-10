7e04e64fdc30e18a03ab64afc847e8f30883f097
MUSTAFA MAZIN AHMED <mustafa.hamedi.2001@gmail.com>
2024-04-10 09:23:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // Compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // If the letters don't match, return false
+      return false;
+    }
+  }
+
+  // If we reach the middle, and all the letters match, return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+// Test cases
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("mom")); // true
+console.log(isPalindrome("abba")); // true
+console.log(isPalindrome("a")); // true
+console.log(isPalindrome("hi")); // false
+console.log(isPalindrome("robot")); // false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
